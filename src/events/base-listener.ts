004efa1bff6d82162d22b643fef44cab3173efbd
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-08-16 21:16:20 -0300

    Package updated

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -7,13 +7,13 @@ interface Event {
 }
 
 export abstract class Listener<T extends Event> {
-  abstract subject: T['subject'];
-  
-  abstract onMessage(data: T['data'], msg: Message): void;
-  
+  abstract subject: T["subject"];
+
+  abstract onMessage(data: T["data"], msg: Message): void;
+
   abstract queueGroupName: string;
 
-  private client: Stan;
+  protected client: Stan;
 
   protected ackWait = 5 * 1000;
 
@@ -27,7 +27,7 @@ export abstract class Listener<T extends Event> {
       .setDeliverAllAvailable()
       .setManualAckMode(true)
       .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName)
+      .setDurableName(this.queueGroupName);
   }
 
   listen() {
@@ -35,21 +35,21 @@ export abstract class Listener<T extends Event> {
       this.subject,
       this.queueGroupName,
       this.subscriptionOptions()
-    )
+    );
 
-    subscription.on('message', (msg: Message) => {
+    subscription.on("message", (msg: Message) => {
       console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
-      
+
       const parsedData = this.parseMessage(msg);
 
       this.onMessage(parsedData, msg);
-    })
+    });
   }
 
-  parseMessage(msg: Message) { 
+  parseMessage(msg: Message) {
     const data = msg.getData();
-    return typeof data === 'string'
+    return typeof data === "string"
       ? JSON.parse(data)
-      : JSON.parse(data.toString('utf-8'))
+      : JSON.parse(data.toString("utf-8"));
   }
 }
